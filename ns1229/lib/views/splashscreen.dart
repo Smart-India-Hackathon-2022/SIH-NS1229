36ae594bc73ccdc730d1842f5dac80c0d307e380
Ramesh M S <msramesh2k1@gmail.com>
2022-07-11 10:19:45 +0530

    splashscreen -ui

--- ns1229/lib/views/splashscreen.dart
+++ ns1229/lib/views/splashscreen.dart
@@ -2,13 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:ns1229/Admin/adminpanel.dart';
-import 'package:ns1229/Admin/department.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:im_animations/im_animations.dart';
 import 'package:ns1229/Admin/login.dart';
-import 'package:ns1229/Admin/loginscreen.dart';
-import 'package:ns1229/homescreen.dart';
-
-import '../Admin/pibprofilescreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -20,8 +16,8 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    // TODO: implement initState
-    Timer(const Duration(seconds: 3), () {
+ 
+    Timer(const Duration(seconds: 5), () {
       Get.to(const PIBLogin());
     });
     super.initState();
@@ -31,8 +27,72 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Image.network("https://i.ibb.co/4RcfSPq/344.png"),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            SizedBox(
+              height: 100,
+              width: MediaQuery.of(context).size.width ,
+              child: Fade(
+              duration: const Duration(seconds: 5),
+                   
+            
+            fadeEffect: FadeEffect.fadeIn,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/images/logo.png"),
+                    const SizedBox(width: 20),
+                     Column(
+            
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Text(
+                    'Press Information Bureau',
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.openSans(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                              color: Colors.black, fontSize: 23, letterSpacing: 0),
+                    ),
+                  ),
+                   Row(
+                     children: [
+                       Text(
+                        'Government of India',
+                        
+                        style: GoogleFonts.dmSans(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                  color: Colors.black, fontSize: 20, letterSpacing: 0),
+                        ),
+                  ),
+                     ],
+                   ),
+                   
+                   
+                       ],
+                     ),
+                  ],
+                )),
+            ),
+            const Spacer(),
+            Text(
+                    'Ministry of Information & Broadcasting',
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.dmSans(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.normal,
+                              color: Colors.black, fontSize: 14, letterSpacing: 0),
+                    ),
+                  ),
+                  const SizedBox(height: 20,)
+          ],
+        ),
       ),
     );
   }
